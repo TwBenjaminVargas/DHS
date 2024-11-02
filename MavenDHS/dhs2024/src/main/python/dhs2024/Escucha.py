6b348f9cec0cfac75100037595ca1febc74c8579
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2024-11-02 10:48:37 -0300

    Agregado tipos compatibles, correccion de errores

--- MavenDHS/dhs2024/src/main/python/dhs2024/Escucha.py
+++ MavenDHS/dhs2024/src/main/python/dhs2024/Escucha.py
@@ -12,12 +12,15 @@ class Escucha (compiladoresListener):
         self.numTokens = 0
         self.numNodos = 0
         self.tabla = TablaSimbolos()
+        #pila de control de tipos
+        self.controlTypeLists = list()
+        
         
     def exitContext(self):
         for idstr in self.tabla.contextos[-1].tabla:
            myvar = self.tabla.contextos[-1].tabla[idstr]
            if not myvar.usado:
-               print("\nAdvertencia:\n\tVariable \"{}\" no utilizada".format(myvar.nombre))
+               print("\nAdvertencia:\n\n\tVariable \"{}\" no utilizada".format(myvar.nombre))
         self.tabla.delContexto()
          
     # Enter a parse tree produced by compiladoresParser#programa.
@@ -30,6 +33,7 @@ class Escucha (compiladoresListener):
         
     # Exit a parse tree produced by compiladoresParser#programa.
     def exitPrograma(self, ctx:compiladoresParser.ProgramaContext):
+        self.exitContext() 
         print("\nFinaliza analisis semantico")
         #print("Se encontraron:")
         #print("\tCantidad de nodos: " + str(self.numNodos))
@@ -37,12 +41,11 @@ class Escucha (compiladoresListener):
         print("*" * 40)
         print("")
         
-        self.exitContext() 
 
     
     # Enter a parse tree produced by compiladoresParser#declaracion.
     def enterDeclaracion(self, ctx:compiladoresParser.DeclaracionContext):
-        print("\n+Declaracion:")
+        print("\n+Declaracion:\n")
         
         
         
@@ -64,135 +67,168 @@ class Escucha (compiladoresListener):
             print(myvar)
         else:
             print(f"-ERROR:\n\tIdentificado repetido {myvar.nombre}")
-            
-
+                
+    
     # Enter a parse tree produced by compiladoresParser#asignacion.
     def enterAsignacion(self, ctx:compiladoresParser.AsignacionContext):
         print("\n+Asignacion")
-
+        self.controlTypeLists.append(list()) #agregamos una lista de control 
+    
+    # Aqui se incluiran algunas reglas para validar una asignacion segun el tipo
+    def validarAsignacion(self,tipodato):
+        if tipodato == 'INT':
+            if 'FLOAT' in self.controlTypeLists[-1]:
+                print("\nError:\n\tIntento de asignacion de FLOAT a INT")
+        if tipodato == 'CHAR':
+            if 'FLOAT' in self.controlTypeLists[-1]:
+                print("\nError:\n\tIntento de asignacion de FLOAT a CHAR")
+      
     # Exit a parse tree produced by compiladoresParser#asignacion.
     def exitAsignacion(self, ctx:compiladoresParser.AsignacionContext):
-        
-        # buscar variable y la establece inicializada
+        # buscar variable
         myvar = self.tabla.buscarGlobal(ctx.getChild(0).getText())
+        # valida asignacion y establece inicializada
         if myvar:
+            self.validarAsignacion(myvar.tipoDato)
             myvar.inicializado = True
+            print("\n\tEstado Variable:\n")
             print(myvar)
         else:    
             print("\n-ERROR:\n\tidentificador no declarado")
+            
+            
+        #eliminamos ultimo elemnto de pila de control
+        print(f"\nTipos usados en esta asignacion: {self.controlTypeLists.pop()}")
+        self.isInAsignacion = False
     
-
+    #Verifica si un ID existe o esta inicializado y lo añade a la controlList
     def verificarID(self,id):
-        if id: # si encuentro un nodo ID
-            myvar = self.tabla.buscarGlobal(id.getText())
-            if myvar:
-                if myvar.inicializado:
-                    myvar.usado = True
-                    print(f"Uso:\n{myvar}")
-                else:
-                    print("\nError:\n\tVariable \"{}\" no inicializada".format(id.getText()))
-            else:
-                print("\nError:\n\tVariable \"{}\" no definida".format(id.getText()))
-    
+        myvar = self.tabla.buscarGlobal(id.getText())
+        if myvar:
+            if not myvar.inicializado:
+                print("\nError:\n\tVariable \"{}\" no inicializada".format(id.getText()))
+            myvar.usado = True
+            self.controlTypeLists[-1].append(myvar.tipoDato)
+        else:
+            print("\nError:\n\tVariable \"{}\" no definida".format(id.getText()))
+            
+    # Añade un tipo de dato a la lista de control
+    def addControlTypeListElement(self,ctx:compiladoresParser.FactorContext):
+        if ctx.getToken(compiladoresParser.NUMERO, 0):
+            self.controlTypeLists[-1].append("INT")
+        if ctx.getToken(compiladoresParser.DECIMAL, 0):
+            self.controlTypeLists[-1].append("FLOAT")
+        if ctx.getToken(compiladoresParser.CARACTER, 0):
+            self.controlTypeLists[-1].append("CHAR")
+            
+                   
     # Exit a parse tree produced by compiladoresParser#factor.
     def exitFactor(self, ctx:compiladoresParser.FactorContext):
+        self.addControlTypeListElement(ctx)
         id = ctx.getToken(compiladoresParser.ID, 0)
-        self.verificarID(id)
+        if id: # si encuentro un nodo ID
+            self.verificarID(id)
         
     # Exit a parse tree produced by compiladoresParser#suf.
     def exitSuf(self, ctx:compiladoresParser.SufContext):
         id = ctx.getToken(compiladoresParser.ID, 0)
-        self.verificarID(id)
+        if id: # si encuentro un nodo ID
+            self.verificarID(id)
     
     # Exit a parse tree produced by compiladoresParser#pref.
     def exitPref(self, ctx:compiladoresParser.PrefContext):
         id = ctx.getToken(compiladoresParser.ID, 0)
-        self.verificarID(id)
-        
-     
-        
-            
-                
+        if id: # si encuentro un nodo ID
+            self.verificarID(id)
         
 
         
  #  Enter a parse tree produced by compiladoresParser#iwhile.
     def enterIwhile(self, ctx:compiladoresParser.IwhileContext):
-        print("\n" + "="*20 + " While " + "="*20)
-        
         # Iniciamos contexto while
         self.tabla.addContexto()
+        print("\n" + "="*20 + " While " + "="*20)
 
     # Exit a parse tree produced by compiladoresParser#iwhile.
     def exitIwhile(self, ctx:compiladoresParser.IwhileContext):
-        print("\n"+ "="*20 + " Fin While "+ "="*20)
         #print("\tCantidad de hijos: " + str(ctx.getChildCount()))
         #print("\tTokens: " +ctx.getText())
-        
         self.exitContext()
+        print("\n"+ "="*20 + " Fin While "+ "="*20)
     
 
     # Enter a parse tree produced by compiladoresParser#ifor.
     def enterIfor(self, ctx:compiladoresParser.IforContext):
-        print("\n" + "="*20 + " For " + "="*20)
-        
         # Iniciamos contexto for
         self.tabla.addContexto()
+        print("\n" + "="*20 + " For " + "="*20)
+        
 
     # Exit a parse tree produced by compiladoresParser#ifor.
     def exitIfor(self, ctx:compiladoresParser.IforContext):
-        print("\n"+ "="*20 + " Fin For "+ "="*20)
         #print("\tCantidad de hijos: " + str(ctx.getChildCount()))
         #print("\tTokens: " +ctx.getText())
-        
         self.exitContext()
+        print("\n"+ "="*20 + " Fin For "+ "="*20)
+        
 
     # Enter a parse tree produced by compiladoresParser#iif.
     def enterIif(self, ctx:compiladoresParser.IifContext):
-        print("\n" + "="*20 + " If " + "="*20)
-        
         # Iniciamos contexto if
         self.tabla.addContexto()
+        print("\n" + "="*20 + " If " + "="*20)
+        
 
     # Exit a parse tree produced by compiladoresParser#iif.
     def exitIif(self, ctx:compiladoresParser.IifContext):
-        print("\n"+ "="*20 + " Fin If "+ "="*20)
         #print("\tCantidad de hijos: " + str(ctx.getChildCount()))
         #print("\tTokens: " +ctx.getText())
         
-        self.exitContext()
+        #if solo elimina su contexto si else no lo elimino
+        if ctx.getChildCount() == 5:
+            self.exitContext()
+            print("\n"+ "="*20 + " Fin If "+ "="*20)
 
 
     # Enter a parse tree produced by compiladoresParser#ielse.
     def enterIelse(self, ctx:compiladoresParser.IelseContext):
-        print("\n" + "="*20 + " Else " + "="*20)
-        
+        #eliminamos contexto if
+        self.exitContext()
+        print("\n"+ "="*20 + " Fin If "+ "="*20)
         # Iniciamos contexto else
         self.tabla.addContexto()
+        print("\n" + "="*20 + " Else " + "="*20)
+        
 
     # Exit a parse tree produced by compiladoresParser#ielse.
     def exitIelse(self, ctx:compiladoresParser.IelseContext):
-        print("\n"+ "="*20 + " Fin Else "+ "="*20)
         #print("\tCantidad de hijos: " + str(ctx.getChildCount()))
         #print("\tTokens: " +ctx.getText())
-        
         self.exitContext()
+        print("\n"+ "="*20 + " Fin Else "+ "="*20)
 
 
     # Enter a parse tree produced by compiladoresParser#ifuncion.
     def enterIfuncion(self, ctx:compiladoresParser.IfuncionContext):
-        print("\n" + "="*20 + " Funcion " + "="*20)
-        
         # Iniciamos contexto funcion
         self.tabla.addContexto()
+        print("\n" + "="*20 + " Funcion " + "="*20)
 
     # Exit a parse tree produced by compiladoresParser#ifuncion.
     def exitIfuncion(self, ctx:compiladoresParser.IfuncionContext):
-        print("\n"+ "="*20 + " Fin Funcion "+ "="*20)
         #print("\tCantidad de hijos: " + str(ctx.getChildCount()))
         #print("\tTokens: " +ctx.getText())
-        
         self.exitContext()
+        print("\n"+ "="*20 + " Fin Funcion "+ "="*20)
+        
+     # Enter a parse tree produced by compiladoresParser#cond.
+    def enterCond(self, ctx:compiladoresParser.CondContext):
+        self.controlTypeLists.append(list())
+
+    # Exit a parse tree produced by compiladoresParser#cond.
+    def exitCond(self, ctx:compiladoresParser.CondContext):
+        print(f"Tipos de datos usados en condicion : {self.controlTypeLists.pop()}")
+
 
     def visitTerminal(self, node: TerminalNode):
         #print("-----> Token: " + node.getText())
