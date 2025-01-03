4a0756c778acd635a81799bd15740940b13542b5
AntoBadami <cantobadami@gmail.com>
2025-01-03 20:52:14 -0300

    agrega comparaciones de terminos

--- MavenDHS/dhs2024/src/main/python/dhs2024/Walker.py
+++ MavenDHS/dhs2024/src/main/python/dhs2024/Walker.py
@@ -9,6 +9,7 @@ class Walker (compiladoresVisitor):
         self.temporales = Temporales()
         self.temporalesTerminales = list()
         self.isSimpleTerm = False
+        self.isInComparacion = False
     def visitPrograma(self,ctx : compiladoresParser.ProgramaContext):
         print("Inicia generacion de codigo intermedio...")
         return super().visitPrograma(ctx)
@@ -26,8 +27,10 @@ class Walker (compiladoresVisitor):
             super().visitFactor(ctx.getChild(0))
             if ctx.getChild(1).getChildCount() > 0: # es parte de un termino complejo
                 self.codigoIntermedio.addLine(f"{self.temporales.generateTemp()} = {self.temporalesTerminales[-1][-1]} {ctx.getChild(1).getChild(0).getText()} {ctx.getChild(1).getChild(1).getText()}")
+                #limpiamos el temporal del termino con parentesis ya que no lo queremos guardar
+                self.temporalesTerminales[-1].pop()
                 self.visitT(ctx.getChild(1).getChild(2))
-                #VER SI GENERA ERROR EN 1*(1*2)*3*4
+                #guardamos el temporal con el resultado del producto
                 self.temporalesTerminales[-1].append(self.temporales.getTop())
             else:
                 self.isSimpleTerm = False
@@ -38,10 +41,10 @@ class Walker (compiladoresVisitor):
         else:
             self.isSimpleTerm = False
         
-        if self.isSimpleTerm and len(self.temporalesTerminales[-1]) == 0:
+        if self.isSimpleTerm and (len(self.temporalesTerminales[-1]) == 0 or self.isInComparacion):
             self.codigoIntermedio.addLine(f"{self.temporales.generateTemp()} = {ctx.getText()}")
         elif not self.isSimpleTerm: #inicia  calculo de producto
-            if "(" in ctx.getChild(1).getChild(1).getText():#se multiplica/divide por un producto
+            if "(" in ctx.getChild(1).getChild(1).getText():#se multiplica/divide por un termino en parentesis
                 self.codigoIntermedio.addLine(f"{self.temporales.generateTemp()} = {ctx.getChild(0).getText()}")
                 self.temporalesTerminales[-1].append(self.temporales.getTop())
                 self.visitExp(ctx.getChild(1).getChild(1).getChild(1)) # calculamos expresion dentro de parentesis
@@ -57,6 +60,7 @@ class Walker (compiladoresVisitor):
             self.visitT(ctx.getChild(1).getChild(2))
         
         self.temporalesTerminales[-1].append(self.temporales.getTop())
+        print(f"Añadi el {self.temporalesTerminales[-1][-1]}")
         return None
     
     def visitExp(self, ctx:compiladoresParser.ExpContext):
@@ -91,5 +95,27 @@ class Walker (compiladoresVisitor):
                 self.codigoIntermedio.addLine(f"{self.temporales.generateTemp()} = {op}")
                 self.visitT(ctx.getChild(2))
         return None
-
-      
+    
+    def visitComp(self, ctx:compiladoresParser.CompContext):
+        """
+         Al visitarse una comparacion se establece una bandera isInComparacion y se calcula
+         el primer termino de la misma, luego se trabaja con el nodo C
+        """
+        super().visitOp(ctx.getChild(0))
+        if ctx.getChild(1).getChildCount() > 0:
+            self.isInComparacion = True
+            self.visitC(ctx.getChild(1))
+        else:
+            self.isInComparacion = False
+        return None
+    
+    def visitC(self, ctx:compiladoresParser.CContext):
+        """
+        Se visista la comparacio de manera recursiva, con la finalidad de calcular el segundo
+        termino de la misma.
+        Luego se procede a aplicar la comparacion de los dos terminos, esto con la finalidad de comparar terminos complejos
+        """
+        self.visitComp(ctx.getChild(1))
+        self.codigoIntermedio.addLine(f"{self.temporales.generateTemp()} = {self.temporalesTerminales[-1][-2]} {ctx.getChild(0).getText()} {self.temporalesTerminales[-1][-1]}")
+        self.temporalesTerminales[-1].append(self.temporales.getTop())
+        return None
