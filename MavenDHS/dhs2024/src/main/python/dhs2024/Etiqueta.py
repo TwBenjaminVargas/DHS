4aac27f8404dc75bea03c1c1dab3fe910e49b871
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-01-04 12:49:59 -0300

    Se agregaron operaciones con variables, y bloque if

--- MavenDHS/dhs2024/src/main/python/dhs2024/Etiqueta.py
+++ MavenDHS/dhs2024/src/main/python/dhs2024/Etiqueta.py
@@ -1,5 +1,18 @@
-class Temporales:
+class Etiqueta:
     
     def __init__(self):
        self.index = 0
-       self.tmp
+       # FIFO de etiquetas
+       self.lbl = list()
+       self.history = list()
+    
+    def generateLabel(self):
+        self.lbl.append(f"l{self.index}")
+        self.history.append(self.lbl[-1])
+        self.index += 1
+        return self.lbl[-1]
+    
+    def getLabel(self):
+        if self.lbl:
+            return self.lbl.pop(0)
+        return None
